60aec3e32fdb3342c36d231d37dbd28df9ab7762
George Tudor <Alexander.George.Tudor@gmail.com>
2022-02-06 23:54:09 -0600

    added basic form and array of links to test

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="hello">
-    <h3>{{ title }}</h3>
+    <h3>{{ header }}</h3>
     <div class="form">
       <div class="form-group">
-        <label>Link Address</label>
-        <input class="form-control" type="text" v-model="link.address">
+        <label>Link Address: </label>
+        <input class="form-control" type="text" v-model="link.url">
+      </div>
+      <div class="form-group">
+        <label>Tags: </label>
+        <input class="form-control" type="text" v-model:="link.tags">
       </div>
     </div>
+    <button class="btn btn-primary">Submit</button>
   </div>
 </template>
 
@@ -15,10 +20,50 @@ export default {
   name: 'Links',
   data() {
     return {
-      title: 'Links',
+      header: 'Links',
       link: {
-        address: '',
-
+        title: '',
+        description: '',
+        image: '',
+        url: '',
+        tags: [],
+        links: [
+          {
+            title: 'align-self | CSS-Tricks',
+            description: 'The align-self property is a sub-property of the Flexible Box Layout module.',
+            image: 'https://css-tricks.com/wp-json/social-image-generator/v1/image/21208',
+            url: 'https://css-tricks.com/almanac/properties/a/align-self/',
+            tags: []
+          },
+          {
+            title: 'Bootstrap 4 Tabs - examples &amp; tutorial. Basic &amp; advanced usage - Material Design for Bootstrap',
+            description: 'Bootstrap tabs are components which separate content placed in the same wrapper but in the separate pane. Only one pane can be displayed at the time.',
+            image: 'https://mdbcdn.b-cdn.net/wp-content/uploads/2016/05/tabs-fb.jpg',
+            url: 'https://mdbootstrap.com/docs/b4/jquery/components/tabs/',
+            tags: []
+          },
+          {
+            title: 'Randall Degges - How to Have Fun Programming',
+            description: 'Having fun programming is something that a lot of programmers forget how to do -- and that makes me sad. Check this shit out.',
+            image: 'https://www.rdegges.com/static/images/favicons/apple-icon-180x180.png',
+            url: 'https://www.rdegges.com/2012/how-to-have-fun-programming/',
+            tags: []
+          },
+          {
+            title: '21 Best Healthy Cooking Hacks of All Time | Eat This Not That',
+            description: 'If you’re looking to make your cooking healthier and convenient, there are a few healthy cooking hacks you can keep in mind for future culinary adventures.',
+            image: 'https://www.eatthis.com/wp-content/uploads/sites/4/2020/08/cooking-shrimp.jpg?fit=1200%2C879&ssl=1',
+            url: 'https://www.eatthis.com/best-healthy-cooking-hacks/',
+            tags: []
+          },
+          {
+            title: 'Journey — Learn WebGL with Three.js',
+            description: 'The ultimate Three.js course whether you are a beginner or a more advanced developer',
+            image: 'https://threejs-journey.com/assets/social/open-graph-image-1200x630.png',
+            url: 'https://threejs-journey.xyz/Three.js',
+            tags: []
+          }
+        ]
       }
     }
   }
